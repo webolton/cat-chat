579f5f31b8c41672fda7b3f111bc347043dc7a84
William Bolton <william.ellet@gmail.com>
2022-03-26 11:47:52 -0400

    Command for running the application

--- config/containers/app_cmd.sh
+++ config/containers/app_cmd.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# Set up and run the database
+set -e
+echo "local    all             all             trust" > /etc/postgresql/*/main/pg_hba.conf
+/etc/init.d/postgresql start
+
 # Set up and run the application
+bundle exec rails db:create RAILS_ENV="$RAILS_ENV"
 bundle exec rails db:migrate RAILS_ENV="$RAILS_ENV"
-RAILS_ENV="$RAILS_ENV" bundle exec puma --daemon --bind unix://tmp/sockets/puma.sock --state tmp/sockets/puma.state --control unix://tmp/sockets/pumactl.sock
+
+# If we were using NGINX
+# RAILS_ENV="$RAILS_ENV" bundle exec puma --bind unix://tmp/sockets/puma.sock \
+#   --state tmp/sockets/puma.state
+
+# Binding puma to TCP
+RAILS_ENV="$RAILS_ENV" bundle exec puma --bind tcp://0.0.0.0:3000 \
+  --state tmp/sockets/puma.state
